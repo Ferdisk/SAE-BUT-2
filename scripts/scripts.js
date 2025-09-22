843f8b1c79fc45d8dffd4b5538b2201324f15e7f
Adrachi Ferdina <ferdina.adrachi16@gmail.com>
2025-09-22 17:17:02 +0200

    Ajoute vérification question avant envoi
    Implémente une vérification pour s'assurer qu'au moins une question a été ajoutée au formulaire avant de permettre l'envoi ou l'effacement. Affiche une alerte si aucune question n'est présente.

---
+++ scripts/scripts.js
@@ -0,0 +1,18 @@
+const ajoutQuestionBtn = document.getElementById('ajout-question-btn');
+const divbtnform_actions = document.querySelector('.form-actions');
+const boutonform_actions = divbtnform_actions.querySelectorAll('button');
+
+//Vérifie s'il y a des questions quand le bouton envoyer et effacer sont clicker
+boutonform_actions.forEach(btn => {
+    btn.addEventListener('click', () => {
+            if (verifiePresenceQuestion()){
+
+            } else {
+                alert("Vous n'avez ajouté aucune question au formulaire !")
+            };
+        });
+    });
+
+function verifiePresenceQuestion() {
+
+}
