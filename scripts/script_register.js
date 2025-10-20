ff88bf01caca25bd9257452badc2caf738af4eb7
Adrachi Ferdina <ferdina.adrachi16@gmail.com>
2025-10-20 14:35:03 +0200

    Ajoute formulaires aux pages d'authentification
    Ajoute les balises de formulaire manquantes aux pages d'inscription et de connexion.
    
    Cela structure les pages pour une future intégration de la logique d'authentification.

---
+++ scripts/script_register.js
@@ -0,0 +1,10 @@
+// function hasNonEmptyChild(element) {
+  
+//   const children = element.children;
+//   for (const child of children) {
+//     if (child.textContent.trim() !== "") {
+//       return true;
+//     }
+//   }
+//   return false; 
+// }
