55ace8e82a859960db121170efd55bcb9c839909
M-begue <begue.matteo24@gmail.com>
2025-10-20 13:17:06 +0200

    Ajout d'un début de php pour le login et modification de la table utilisateurs (suppression des colonnes nom et prénom)

--- BDD/utilisateur.sql
+++ BDD/utilisateur.sql
@@ -2,9 +2,9 @@
 use sae_db;
 CREATE TABLE utilisateurs (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    nom VARCHAR(100) NOT NULL,
-    prenom VARCHAR(100) NOT NULL,
     email VARCHAR(150) UNIQUE NOT NULL,
     mdp_hash VARCHAR(255) NOT NULL,
     role VARCHAR(20) check (role in ('enseignant','etudiant')) NOT NULL
 );
+
+SELECT * FROM utilisateurs;
