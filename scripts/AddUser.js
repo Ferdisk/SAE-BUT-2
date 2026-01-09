0556ec4a13d168cb703bbb810ac57e5506391872
hchevaldonnet <hubert.chevaldonnet@etu.unilim.fr>
2026-01-09 14:46:37 +0100

    Ajout des utilisateurs en tant qu'admin

--- scripts/AddUser.js
+++ scripts/AddUser.js
@@ -1,22 +1,52 @@
+document.querySelector('.add-user').addEventListener('click', function() {
+    const modal = new bootstrap.Modal(document.getElementById('addUserModal'));
+    modal.show();
+});
 
-    document.querySelector('.add-user').addEventListener('click', function() {
-      const modal = new bootstrap.Modal(document.getElementById('addUserModal'));
-      modal.show();
-    });
+document.getElementById('submitUser').addEventListener('click', async function() {
+    const newUserMail = document.getElementById('newUserMail').value;
+    const newUserPassword = document.getElementById('newUserPassword').value;
+    const newUserPasswordConfirm = document.getElementById('newUserConfirm').value;
+    const feedback = document.getElementById('feedback');
+    feedback.style.color = "red";
+    if (!newUserMail || !newUserPassword) {
+	feedback.textContent="Veuillez remplir tous les champs.";
+	return;
+    }
+    if (newUserPassword != newUserPasswordConfirm) {
+	feedback.textContent="Les mots de passe ne sont pas similaires.";
+	return;
+    }
 
-    // Gérer la soumission du formulaire
-    document.getElementById('submitUser').addEventListener('click', function() {
-      const studentName = document.getElementById('studentName').value;
-      const studentSurName = document.getElementById('studentSurName') ? document.getElementById('studentSurName').value : '';
-      if (studentName.trim() !== '') {
-        console.log('Nom de l\'étudiant:', studentName);
-        console.log('Prénom de l\'étudiant:', studentSurName);
-        // Ici vous pouvez ajouter votre logique pour sauvegarder l'utilisateur
-        const modal = bootstrap.Modal.getInstance(document.getElementById('addUserModal'));
-        modal.hide();
-        // Réinitialiser le formulaire
-        document.getElementById('addUserForm').reset();
-      } else {
-        alert('Veuillez entrer un nom');
-      }
-    });
+    let role = '';
+    if (newUserMail.includes("@etu.unilim.fr")) {
+	role = 'Etudiant';
+    } else if (newUserMail.includes("@unilim.fr") || newUserMail.includes("aff@unilim.fr")) {
+	role = 'Prof';
+    } else {
+	feedback.textContent="Email invalide.";
+	return;
+    }
+
+    alert('mail :' + newUserMail);
+    alert('mdp:' + newUserPassword);
+
+    const response = await fetch("http://164.81.120.71:3000/addUser", {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            credentials: "include",
+            body: JSON.stringify({ newUserMail, newUserPassword, role})
+        });
+
+        const data = await response.json();
+
+        if (data.success) {
+            feedback.style.color = "green";
+            feedback.textContent = data.message;
+    	    document.getElementById('addUserForm').reset();
+        } else {
+            feedback.style.color = "red";
+            feedback.textContent = data.message;
+        }
+
+});
